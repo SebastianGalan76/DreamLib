912fb965b8b09bb8f6746b3ca42ebdf7e57bc7b9
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-03-09 13:46:28 +0100

    Added function to load item flags

--- src/main/java/pl/dream/dreamlib/Config.java
+++ src/main/java/pl/dream/dreamlib/Config.java
@@ -10,6 +10,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,40 +24,55 @@ public class Config {
      * @return A ItemStack retrieved from the config, or null if there is an error in the config
      */
     public static @Nullable ItemStack getItemStack(@NotNull FileConfiguration config, @NotNull String path){
+        //Loading material
         Material material = getMaterial(config, path+".material");
         if(material==null){
             return null;
         }
 
+        //Loading amount of item
         int amount = config.getInt(path+".amount", 1);
         if(amount<=0){
             Bukkit.getLogger().warning("Incorrect item amount: "+path);
             return null;
         }
 
+        //Create ItemStack and ItemMeta
         ItemStack item = new ItemStack(material, amount);
         ItemMeta itemMeta = item.getItemMeta();
 
+        //Loading item name
         String name = config.getString(path+".name");
         if(name!=null){
             itemMeta.setDisplayName(Color.fixRGB(name));
         }
 
+        //Loading lore
         if(config.getString(path+".lore")!=null){
             List<String> lore = Color.fixRGB(config.getStringList(path+".lore"));
             itemMeta.setLore(lore);
         }
 
+        //Loading glowing
         boolean glowing = config.getBoolean(path+".glowing", false);
         if(glowing){
             itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
             itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         }
 
+        //Loading enchantments
         HashMap<Enchantment, Integer> enchantments = getEnchantments(config, path+".enchant");
         if(enchantments!=null && !enchantments.isEmpty()){
             enchantments.forEach((enchant, level) -> {
                 itemMeta.addEnchant(enchant, level, true);
+            });
+        }
+
+        //Loading item flags
+        List<ItemFlag> itemFlagList = getItemFlags(config, path+".itemFlag");
+        if(itemFlagList!=null && !itemFlagList.isEmpty()){
+            itemFlagList.forEach((itemFlag) -> {
+                itemMeta.addItemFlags(itemFlag);
             });
         }
 
@@ -95,12 +111,12 @@ public class Config {
     public static @Nullable HashMap<Enchantment, Integer> getEnchantments(@NotNull FileConfiguration config, String enchantsPath){
         HashMap<Enchantment, Integer> enchantments = new HashMap<>();
 
-        List<String> enchantsString = config.getStringList(enchantsPath);
-        if(enchantsString.isEmpty()){
+        List<String> enchantsStringList = config.getStringList(enchantsPath);
+        if(enchantsStringList.isEmpty()){
             return null;
         }
 
-        for(String enchant:enchantsString){
+        for(String enchant:enchantsStringList){
             String[] parts = enchant.split(":");
 
             String enchantName = parts[0];
@@ -121,5 +137,36 @@ public class Config {
         }
 
         return enchantments;
+    }
+
+    /**
+     * Retrieves a list of ItemFlags from the specified configuration using the provided path
+     *
+     * @param config The config from which to retrieve the enchantments
+     * @param itemFlagPath The path in the configuration where the ItemFlags are located
+     * @return A list of materials retrieved from the config, or null if no item flags are found
+     */
+    public static @Nullable List<ItemFlag> getItemFlags(@NotNull FileConfiguration config, String itemFlagPath){
+        List<ItemFlag> itemFlagList = new ArrayList<>();
+
+        List<String> itemFlagStringList = config.getStringList(itemFlagPath);
+        if(itemFlagStringList.isEmpty()){
+            return null;
+        }
+
+        for(String itemFlagString:itemFlagStringList){
+            ItemFlag itemFlag;
+
+            try{
+                itemFlag = ItemFlag.valueOf(itemFlagString);
+            }catch (IllegalArgumentException e){
+                Bukkit.getLogger().warning("Incorrect item flag: "+ itemFlagPath +"\n"+e.getMessage());
+                continue;
+            }
+
+            itemFlagList.add(itemFlag);
+        }
+
+        return itemFlagList;
     }
 }
