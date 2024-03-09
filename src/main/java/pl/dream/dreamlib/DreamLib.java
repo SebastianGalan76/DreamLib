c92b367f48e99e3bf748fdfea32c2e188f4b7e6c
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-03-09 11:18:01 +0100

    initial project

---
+++ src/main/java/pl/dream/dreamlib/DreamLib.java
@@ -0,0 +1,17 @@
+package pl.dream.dreamlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DreamLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
